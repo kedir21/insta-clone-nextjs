1d3af3144006db84059303823ed04ff49b036d7a
KEDIR NASIR <101452514+kedir21@users.noreply.github.com>
2023-04-16 23:15:38 +0300

    added nextauthsecret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,22 +1,14 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 export const authOptions = {
-  // Configure one or more authentication providers
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // ...add more providers here
 ],
-
-
-// theme: {
-//     logo:"https://links.papareact.com/sq0",
-//     brandColor:"#F13287",
-//     colorScheme:"auto",
-// },
-
+secret:process.env.NEXTAUTH_SECRET,
 pages: {
     signIn:"/auth/signin",
 },
