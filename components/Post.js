2bc6ee6bdb799a758d3c31ca9e17537970fd3f6a
KEDIR NASIR <101452514+kedir21@users.noreply.github.com>
2023-04-17 09:42:50 +0300

    third commit updating the comment section

--- components/Post.js
+++ components/Post.js
@@ -12,9 +12,10 @@ import Moment from 'react-moment';
 import {HeartIcon as HeartIconFilled} from '@heroicons/react/solid'
 import { useSession } from 'next-auth/react';
 import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, serverTimestamp, setDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
+import { db } from '../firebase';
 
-function Post({id, img, userImg, caption, username}) {
+function Post({id, username, userImg, img, caption}) {
+
   const {data:session} = useSession();
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
@@ -124,9 +125,13 @@ function Post({id, img, userImg, caption, username}) {
             {comments.map(comment => (
               <div key={comment.id} className='flex items-center space-x-2 mb-3'>
                   <img className='h-7 rounded-full' src={comment.data().userImage} alt='' />
-                  <p className='text-sm flex-1'><span className='font-bold'>{comment.data().username}</span>{comment.data().comment}</p>
-                  <Moment>
-                    {comment.data().timestamp}
+                  <p className='text-sm flex-1'>
+                   <span className='font-bold'>{comment.data().username}
+                  </span>{" "}
+                    {comment.data().comment}
+                  </p>
+                  <Moment fromNow className='pr-5 text-xs'>
+                    {comment.data().timestamp?.toDate()}
                   </Moment>
               </div>
             ))
@@ -141,10 +146,16 @@ function Post({id, img, userImg, caption, username}) {
             <EmojiHappyIcon className='h-7'/> 
             <input type='text' 
             value={comment}
-            onChange={(e) => setComment (e.target.value)}
+            onChange={e => setComment (e.target.value)}
+            placeholder='add a comment ...' 
+            className='border-none flex-1 focus:ring-0 outline-none'/>
+            <button 
+            disabled={!comment.trim()} 
             onClick={sendComment}
-            placeholder='add a comment ...' className='border-none flex-1 focus:ring-0 outline-none'/>
-            <button disabled={!comment.trim()} type="submit" className='font-semibold text-blue-400'>Post</button>
+            type="submit" 
+            className='font-semibold text-blue-400'>
+            Post
+            </button>
         </form>
 )}
       
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Header from '../components/Header'
-import Feed from '../components/Feed'
-import Modal from '@component/components/Modal'
+import Header from '../../components/Header'
+import Feed from '../../components/Feed'
+import Modal from '../../components/Modal'
 
 export default function Home() {
   return (
